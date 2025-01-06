463e286cb8317ece0f28b4b9d0f7eb74eb33027f
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-01-06 22:03:04 +0800

    day 1 lesson success

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
 const express = require("express");
-const fs = require("node:fs");
 const app = express();
+const fs = require("node:fs");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hi");
-});
-// Create
+// create
 app.get("/create", (req, res) => {
-  const date = Date.now();
   const name = req.query.name;
-  const data = fs.readFileSync("data.json", "utf8");
-  const movies = JSON.parse(data);
-  movies.push({ id: date, name });
-  const content = JSON.stringify(movies, null, 4);
-  fs.writeFileSync("data.json", content);
-
-  res.send("success");
-});
-// update
-app.get("/update", (req, res) => {
-  const data = fs.readFileSync("data.json", "utf8");
-  const changingName = req.query.changingName;
-  const changingId = req.query.changingId;
-  const currentName = req.query.name;
-
-  const movies = JSON.parse(data);
-  const findTheOne = movies.filter((movie) => movie.id === changingId);
-  const filterOthers = movies.filter((movie) => movie.name !== currentName);
-  const edit = { ...findTheOne, name: changingName };
-  filterOthers.push(edit);
-
-  const content = JSON.stringify(filterOthers);
-  fs.writeFileSync("data.json", content);
+  const data = fs.readFileSync("data.json");
+  const content = JSON.parse(data);
+  content.push({ id: Date.now(), name });
+  const contentJSON = JSON.stringify(content);
+  fs.writeFileSync("data.json", contentJSON);
   res.send("done");
 });
 
+// read
+app.get("/", (req, res) => {
+  const data = fs.readFileSync("data.json");
+  const content = JSON.parse(data);
+  console.log(content);
+  res.send(content);
+});
+
+// update
+app.get("/update", (req, res) => {
+  const deezId = req.query.deezId;
+  const deezName = req.query.deezName;
+  const data = fs.readFileSync("data.json");
+  const content = JSON.parse(data);
+
+  const movies = content.map((movie) => {
+    if (movie.id == deezId) {
+      const edit = {
+        ...movie,
+        name: deezName,
+      };
+      return edit;
+    } else {
+      return movie;
+    }
+  });
+  console.log(movies);
+
+  const moviesJSON = JSON.stringify(movies);
+  fs.writeFileSync("data.json", moviesJSON);
+  res.send("done");
+});
+
+// delete
+app.get("/delete", (req, res) => {
+  const deezId = req.query.chosenId;
+  const data = fs.readFileSync("data.json");
+  const content = JSON.parse(data);
+
+  const movies = content.filter((movie) => {
+    if (movie.id == deezId) {
+      return;
+    } else {
+      return movie;
+    }
+  });
+
+  const moviesJSON = JSON.stringify(movies);
+  fs.writeFileSync("data.json", moviesJSON);
+  res.send("done nuts");
+});
+
 app.listen(port, () => {
-  console.log(`listening http://localhost:${port}`);
+  console.log(`read---------- http://localhost:3000/`);
+  console.log(
+    `create---------- http://localhost:3000/create?name=randomName${Math.floor(
+      Math.random() * 100000
+    )}`
+  );
+  console.log(
+    `update------------- http://localhost:3000/update?deezId=&deezName=`
+  );
+  console.log(`delte-------------- http://localhost:3000/delete?chosenId=`);
 });
